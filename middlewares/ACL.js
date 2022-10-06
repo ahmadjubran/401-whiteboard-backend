91bd987e2e6b657ce6c8cc1dcadfc8ca1b4b0a62
ahmadjubran <ahmadjubran.98@hotmail.com>
2022-10-06 23:51:03 +0300

    Fix ACL

--- middlewares/ACL.js
+++ middlewares/ACL.js
@@ -5,15 +5,13 @@ const { Post } = require("../models/index");
 const acl = (capability) => {
   return async (req, res, next) => {
     try {
-      if (!req.user.capabilities.includes(capability) && !req.user.id) {
-        next("Access Denied");
-      }
       if (req.user.capabilities.includes(capability)) {
         next();
-      }
-      if (req.params.id) {
+      } else if (req.params.id) {
         const post = await Post.read(req.params.id);
         post.userId === req.user.id ? next() : next("Access Denied");
+      } else {
+        next("Access Denied");
       }
     } catch (e) {
       next("Invalid Login");
